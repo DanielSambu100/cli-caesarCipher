7c7cc9ae86bd9de1bf03b24022baa6e85943d07b
DanielSambu100 <daniel.wanyonyi@stdent.moringaschool.com>
2022-05-08 19:04:42 +0300

    update encryption class file

--- src/main/java/caesarCipher/Encryption.java
+++ src/main/java/caesarCipher/Encryption.java
@@ -1,15 +1,15 @@
 package caesarCipher;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Encryption {
-private String text;
-    public Encryption() {
-
-    }
-
-    public  static Scanner encrypt(String text, int key){
-        Scanner result = new Scanner(System.in);
+//A Java Program to illustrate Caesar Cipher Technique
+class Encryption {
+    // Encrypts plain text with a key
+    public static StringBuffer encrypt(String text, int key)
+    {
+        StringBuffer result= new StringBuffer();
 
         for (int i=0; i<text.length(); i++)
         {
@@ -17,25 +17,26 @@ private String text;
             {
                 char ch = (char)(((int)text.charAt(i) +
                         key - 65) % 26 + 65);
-                result.next().charAt(ch);
+                result.append(ch);
             }
             else
             {
                 char ch = (char)(((int)text.charAt(i) +
                         key - 97) % 26 + 97);
-                result.next().charAt(ch);
+                result.append(ch);
             }
         }
         return result;
     }
 
-    public static void main(String[] args) {
-Scanner keyboard = new Scanner(System.in);
-System.out.println("Welcome to CaesarCipher Encryption");
-System.out.println("Please enter your plain text for encryption");
-String text = keyboard.next();
-System.out.println("Please enter your preferred key");
-Integer key = keyboard.nextInt();
-System.out.println("Your cipher text is "+ encrypt(text,key));
+    // Driver code
+    public static void main(String[] args) throws IOException {
+        InputStreamReader streamReader = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(streamReader);
+        System.out.println("Please enter your plain text");
+        String text = bufferedReader.readLine();
+        System.out.println("Please enter your key between 1 and 25");
+        Integer key = Integer.valueOf(bufferedReader.readLine());
+        System.out.println("Your cipher text is "+encrypt(text,key));
     }
 }
