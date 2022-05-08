a58182d77746cce1aa83167531e120d96aa2c3f7
DanielSambu100 <daniel.wanyonyi@stdent.moringaschool.com>
2022-05-08 18:02:50 +0300

    update Decryption class file

--- src/main/java/caesarCipher/Decryption.java
+++ src/main/java/caesarCipher/Decryption.java
@@ -1,4 +1,11 @@
 package caesarCipher;
 
+import java.util.Scanner;
+
 public class Decryption {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+
+    }
 }
