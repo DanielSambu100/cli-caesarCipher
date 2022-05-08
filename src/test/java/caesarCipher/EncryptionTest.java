b935337e46c90d477d23a62a8a46f6f9e8f0b01e
DanielSambu100 <daniel.wanyonyi@stdent.moringaschool.com>
2022-05-08 19:17:37 +0300

    update encryptiontest class file

---
+++ src/test/java/caesarCipher/EncryptionTest.java
@@ -0,0 +1,32 @@
+package caesarCipher;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EncryptionTest {
+
+    @Test
+    void getText() {
+    }
+
+    @Test
+    void setText() {
+    }
+
+    @Test
+    void getKey() {
+    }
+
+    @Test
+    void setKey() {
+    }
+
+    @Test
+    void encrypt() {
+    }
+
+    @Test
+    void main() {
+    }
+}
